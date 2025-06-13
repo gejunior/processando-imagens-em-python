8ed856a3ed6549f6ea17fe258a58b9ca97d93f4f
genilson <genilson_junior14@outlook.com>
2025-06-13 11:54:49 -0300

    instalacoes terminal

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
-    page_description - f.read()
+    page_description = f.read()
 
 with open("requeriments.txt") as f:
-    requeriments = f.read().splitlines()
+    requeriments = [line.strip() for line in f.read().splitlines() if line.strip()]
+    # requeriments = f.read().splitlines()
 
 setup(
     name="image_processing",
     version="0.0.1",
-    author="Genilson Junior"
-    description="Processamento de cores nas imagens"
+    author="Genilson Junior",
+    description="Processamento de cores nas imagens",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gejunior/processando-imagens-em-python.git"
+    url="https://github.com/gejunior/processando-imagens-em-python.git",
+    packages=find_packages(),
+    install_requires=requeriments,
+    python_requires='>=3.8',
 )
